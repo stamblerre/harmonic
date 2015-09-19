b4a7447c53949d68460320e0a8c996d0c43af84f
stamblerre <rls462@cornell.edu>
2015-09-18 23:29:08 -0400

    Basic app

--- Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
+++ Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
@@ -1,50 +1,67 @@
 package bigredhacks.harmonic;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static String MUSIC_LOG_FILE = "music_log_file"
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /** Callback to start app */
-    public void onMainButtonClick(View view) {
-        Button mainButton = (Button) findViewById(R.id.main_button);
-        if (mainButton.getText().equals(getString(R.string.start))) {
-            // Start ListeningActivity
-        }
-        else {
+    // Start listening to music
+    public static void startListening() {
+        MediaRecorder mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(SONG_FILE);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    }
 
-        }
+    // Stop listening to music and persist data
+    private void stopListening() {
+        Log.d("stopListening", "Clicked stop!");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+        //return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_start:
+                final CharSequence itemTitle = item.getTitle();
+                final String startString = getResources().getString(R.string.start);
+                final String stopString = getResources().getString(R.string.stop);
+                if (itemTitle.equals(startString)) {
+                    item.setTitle(stopString);
+                    startListening();
+                } else if (itemTitle.equals(stopString)) {
+                    item.setTitle(startString);
+                    stopListening();
+                }
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
