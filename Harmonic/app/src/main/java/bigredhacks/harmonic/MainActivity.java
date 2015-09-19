cbfda30b3e14e505b55f5a74453bc26216dc3440
stamblerre <rls462@cornell.edu>
2015-09-19 17:52:58 -0400

    Because apparently I can't let things go - mostly code cleanup

--- Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
+++ Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
@@ -1,33 +1,29 @@
 package bigredhacks.harmonic;
 
-import android.app.ActionBar;
+import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.*;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.LayoutInflater;
-import android.content.Context;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Song;
 import com.echonest.api.v4.Track;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String SONG_FILE = "song.mp4";
     private static final String API_KEY = "6FTQBUXCB2QEJN2IU";
 
-    private final AtomicBoolean listening = new AtomicBoolean(false);
+    private AtomicBoolean listening = new AtomicBoolean(false);
     private final EchoNestAPI echoNest = new EchoNestAPI(API_KEY);
 
     private Song currentSong = null;
@@ -49,9 +45,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Start listening to music
     public void startListening() {
-        mediaRecorder = new MediaRecorder();
+        this.listening.set(true);
+        while (this.listening.get()) {
+            Log.d(LOG_TAG, "I'm listening!");
+            listen();
+            try {
+                Thread.sleep(90000);
+            }
+            catch (final InterruptedException e) {
+                Log.d(LOG_TAG, "Error sleeping.");
+            }
+        }
+    }
+
+    private void playSong(final File songFile) {
+        Log.d(LOG_TAG, "Let's play the song!");
+        final Uri fileURI = Uri.fromFile(songFile);
+        this.mediaPlayer = MediaPlayer.create(this, fileURI);
+        this.mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(final MediaPlayer mPlayer) {
+                mPlayer.start();
+                Log.d(LOG_TAG, "Playing music!");
+            }
+        });
+    }
+
+    /** Start listening to music */
+    private void listen() {
+        Log.d(LOG_TAG, "In listen!");
+        this.mediaRecorder = new MediaRecorder();
         final String songFile = getFilesDir().getAbsolutePath() + File.separator + SONG_FILE;
         Log.d(LOG_TAG, songFile);
 
@@ -59,11 +83,11 @@ public class MainActivity extends AppCompatActivity {
         this.mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         this.mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         this.mediaRecorder.setOutputFile(songFile);
-        this.listening.set(true);
 
         try {
-            mediaRecorder.prepare();
-        } catch (final IOException e) {
+            this.mediaRecorder.prepare();
+        }
+        catch (final IOException e) {
             Log.d(LOG_TAG, "Unable to prepare recording device: " + e.getMessage());
         }
 
@@ -72,98 +96,65 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             Thread.sleep(20000);
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
+        }
+        catch (final InterruptedException e) {
+            Log.d(LOG_TAG, "Unable to sleep for 2 seconds: " + e.getMessage());
         }
 
-        mediaRecorder.stop();
-
-        mediaRecorder.release();
-
-        /*while (this.listening.get()) {
-            /*try {
-                Thread.sleep(2400000);
-            }
-            catch (final InterruptedException e) {
-                Log.d(LOG_TAG, "Unable to sleep: " + e.getMessage());
-            }*/
-            //Log.d(LOG_TAG, "Listening!!!!");
-            //processSong();
-        //}
-        processSong();
+        this.mediaRecorder.stop();
+        this.mediaRecorder.release();
+        this.mediaRecorder = null;
 
         Log.d(LOG_TAG, "Media recorder released!");
+
+        //processSong();
+        playSong(new File(songFile));
     }
 
     private void processSong() {
         try {
-            Log.d(LOG_TAG, "Process song!");
-
+            Log.d(LOG_TAG, "Processing song.");
             final File songFile = new File(getFilesDir().getAbsolutePath() + File.separator + SONG_FILE);
-            Log.d(LOG_TAG, songFile.toString());
+
             final Track track = this.echoNest.uploadTrack(songFile);
-            Log.d(LOG_TAG, "Post track");
-            track.waitForAnalysis(30000);
-            Log.d(LOG_TAG, "After analysis");
-            if (track.getStatus() == Track.AnalysisStatus.COMPLETE) {
+            final Track.AnalysisStatus analysisStatus = track.waitForAnalysis(30000);
+            if (analysisStatus == Track.AnalysisStatus.COMPLETE) {
                 final Song song = new Song(this.echoNest, track.getSongID());
                 Log.d(LOG_TAG, "Song is: " + song.getTitle() + " by " + song.getArtistName());
-
                 if (!song.equals(currentSong)) {
                     logNewSong(song);
-
                 }
             } else {
                 Log.d(LOG_TAG, "Ok something");
             }
-            /*final Uri fileURI = Uri.fromFile(songFile);
-            Log.d(LOG_TAG, songFile.toString());
-            Log.d(LOG_TAG, fileURI.toString());
-            Log.d(LOG_TAG, "Exists: " + songFile.exists());
-            mediaPlayer = MediaPlayer.create(this, fileURI);
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });*/
-        }
-        catch (final Exception e) {
-            e.printStackTrace();
-        }
-        /*catch (final InterruptedException e) {
-            Log.d(LOG_TAG, "Unable to sleep: " + e.getMessage());
         }
         catch (final EchoNestException e) {
-            Log.d(LOG_TAG, "Unable to analyze song: " + e.getMessage());
-        }*/
-    }
-    private void logShit(){
-
-
-
-    }
-private void logNewSong(final Song song) {
-    runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-            String title = song.getTitle();
-            String artist = song.getArtistName();
-
-            LinearLayout v = (LinearLayout) findViewById(R.id.linearLayout1);
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
-            );
-            TextView newText = new TextView(v.getContext());
-            newText.setText(title + " " + artist);
-
-            v.addView(newText, params);
-
-
+            Log.d(LOG_TAG, "Error with EchoNestAPI: " + e.getMessage());
         }
-    });
-}
+        catch (final IOException e) {
+            Log.d(LOG_TAG, "Unable to load file: " + e.getMessage());
+        }
+    }
+
+    private void logNewSong(final Song song) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final String title = song.getTitle();
+                final String artist = song.getArtistName();
+
+                final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
+
+                final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                final TextView newText = new TextView(linearLayout.getContext());
+                newText.setText(title + " " + artist);
+
+                linearLayout.addView(newText, params);
+            }
+        });
+    }
 
     // Stop listening to music and persist data
     private void stopListening() {
@@ -176,7 +167,6 @@ private void logNewSong(final Song song) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
-        //return true;
     }
 
     @Override
@@ -196,7 +186,6 @@ private void logNewSong(final Song song) {
                     item.setTitle(stopString);
                     new Thread(new Runnable() {
                         public void run(){
-
                             startListening();
                         }
                     }).start();
