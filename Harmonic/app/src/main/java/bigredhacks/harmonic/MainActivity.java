ed063c269491cac987076b3c87d0cda73570f6d0
stamblerre <rls462@cornell.edu>
2015-09-19 14:42:32 -0400

    latest?

--- Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
+++ Harmonic/app/src/main/java/bigredhacks/harmonic/MainActivity.java
@@ -1,8 +1,13 @@
 package bigredhacks.harmonic;
 
 import android.app.ActionBar;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.*;
@@ -20,13 +25,15 @@ import com.echonest.api.v4.Song;
 import com.echonest.api.v4.Track;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "MainActivityTest";
     private static final String MUSIC_LOG = "music-log.txt";
-    private static final String SONG_FILE = "song.3gp";
+    private static final String SONG_FILE = "song.mp4";
     private static final String API_KEY = "6FTQBUXCB2QEJN2IU";
 
     private final AtomicBoolean listening = new AtomicBoolean(false);
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Song currentSong = null;
     private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -62,30 +70,41 @@ public class MainActivity extends AppCompatActivity {
         this.mediaRecorder.start();
         Log.d(LOG_TAG, "Started media recorder.");
 
-        while (this.listening.get()) {
+        try {
+            Thread.sleep(20000);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.stop();
+
+        mediaRecorder.release();
+
+        /*while (this.listening.get()) {
             /*try {
                 Thread.sleep(2400000);
             }
             catch (final InterruptedException e) {
                 Log.d(LOG_TAG, "Unable to sleep: " + e.getMessage());
             }*/
-            Log.d(LOG_TAG, "Listening!!!!");
-            processSong();
-        }
-
-        this.mediaRecorder.release(); // TODO Will Android release this if the app is killed without calling stopListening()?
+            //Log.d(LOG_TAG, "Listening!!!!");
+            //processSong();
+        //}
+        processSong();
 
         Log.d(LOG_TAG, "Media recorder released!");
     }
 
     private void processSong() {
         try {
-            //Thread.sleep(20000);
             Log.d(LOG_TAG, "Process song!");
-            final File songFile = new File(getFilesDir().getAbsolutePath() + File.separator + SONG_FILE);
-            final Track track = this.echoNest.uploadTrack(songFile);
 
+            final File songFile = new File(getFilesDir().getAbsolutePath() + File.separator + SONG_FILE);
+            Log.d(LOG_TAG, songFile.toString());
+            final Track track = this.echoNest.uploadTrack(songFile);
+            Log.d(LOG_TAG, "Post track");
             track.waitForAnalysis(30000);
+            Log.d(LOG_TAG, "After analysis");
             if (track.getStatus() == Track.AnalysisStatus.COMPLETE) {
                 final Song song = new Song(this.echoNest, track.getSongID());
                 Log.d(LOG_TAG, "Song is: " + song.getTitle() + " by " + song.getArtistName());
@@ -94,17 +113,30 @@ public class MainActivity extends AppCompatActivity {
                     logNewSong(song);
 
                 }
+            } else {
+                Log.d(LOG_TAG, "Ok something");
             }
+            /*final Uri fileURI = Uri.fromFile(songFile);
+            Log.d(LOG_TAG, songFile.toString());
+            Log.d(LOG_TAG, fileURI.toString());
+            Log.d(LOG_TAG, "Exists: " + songFile.exists());
+            mediaPlayer = MediaPlayer.create(this, fileURI);
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });*/
+        }
+        catch (final Exception e) {
+            e.printStackTrace();
         }
         /*catch (final InterruptedException e) {
             Log.d(LOG_TAG, "Unable to sleep: " + e.getMessage());
-        }*/
-        catch (final IOException e) {
-            Log.d(LOG_TAG, "Unable to read file: " + e.getMessage());
         }
         catch (final EchoNestException e) {
             Log.d(LOG_TAG, "Unable to analyze song: " + e.getMessage());
-        }
+        }*/
     }
     private void logShit(){
 
@@ -164,6 +196,7 @@ private void logNewSong(final Song song) {
                     item.setTitle(stopString);
                     new Thread(new Runnable() {
                         public void run(){
+
                             startListening();
                         }
                     }).start();
